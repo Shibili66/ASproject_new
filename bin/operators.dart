308d66ee1a4b0609aaa21df8f5892f7682fff97a
shibili66 <shibilivesala26@gmail.com>
2022-09-22 15:55:03 +0530

    commit1

--- bin/operators.dart
+++ bin/operators.dart
@@ -1,13 +1,24 @@
-void main()
-{
-  int a=10,b=20;
+void main() {
+  dynamic a = 10,
+      b = 20;
 
   print('arithamatic oprations');
-  print('a+b =${a+b}');
-  print('a-b =${a-b}');
-  print('a*b =${a*b}');
-  print('a/b =${a/b}');
-  print('a~/b =${a~/b}');
-  print('a%b =${a%b}');
-  print('-(a-b) =${-(a-b)}');
+  print('a+=b =${a += b}');
+  print('a-=b =${a -= b}');
+  print('a*=b =${a *= b}');
+  print('a/=b =${a /= b}');
+  print('a~/=b =${a ~/= b}');
+  print('a%=b =${a %= b}');
+  print('-(a-=b) =${-(a -= b)}');
+
+  print('*************************************************');
 }
+ main() {
+  int a = 15;
+
+  print('a++');
+  print('a');
+}
+
+
+
