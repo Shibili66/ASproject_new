12b1b15c531c7e12da3fbc51ed70a6a8b313e13b
shibili66 <shibilivesala26@gmail.com>
2022-09-25 10:19:59 +0530

    commit1

--- bin/oprator2.dart
+++ bin/oprator2.dart
@@ -48,6 +48,34 @@ main() {
   print(l >> 2);
   print(l << 3);
 
+  dynamic data = 20;
+  print(data is String);
+
+  print('ternary operator');
+  ///syx -> condition ? true statment : false statement ;
+  var age = 20;
+  var result = age >= 18 ? "wel to vote " : "not elegi";
+  print(result);
+
+  var mark = 40;
+  var result = mark < 40 ? "sorry u faild the exam " : "passed exam";
+  print(result);
+
+
+
+
+
+
+
+/// largest of 2 numbers
+  var largest = (20 > 30) ? 20 : 30;
+  print('$largest is largest');
+
+
+  var largest = (20 > 30 > 40) ? 20 : 30 : 40;
+  print('$largest is largest');
+
+  int u =3, v=4, w=6;
 
 }
 //
---
+++ bin/userinput.dart
@@ -0,0 +1,19 @@
+void main() {
+  dynamic data = 20;
+  print(data is String);
+
+  print('ternary operator');
+
+  var age = 20;
+  var result = age >= 18 ? "welcome to vote" : "not eligible to vote";
+  print(result);
+  var mark = 40;
+  var result1 = mark < 40 ? "sorry , u failed the test" : "congratulation";
+  print(result1);
+
+  var un = 'admin';
+  var pw = 1234;
+
+  var login = (un == 'admin' && pw == 1234) ? "login successful" : 'login failure';
+  print(login);
+}
