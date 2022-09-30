bbb0281959288443bdc89a91ce5c49cff82be969
shibili66 <shibilivesala26@gmail.com>
2022-09-30 16:11:49 +0530

    commit1

--- bin/listexm.dart
+++ bin/listexm.dart
@@ -1,18 +1,18 @@
-void main(){
-  List names = ['ammu','achu','appu'];
+void main() {
+  List names = ['ammu', 'achu', 'appu'];
   names.add('amma');
   print(names);
 
-///list.empty()
+  ///list.empty()
   List List1 = List.empty(growable: true);
   List1.add(20);
   print(List1);
 
-///list.filled
+  ///list.filled
   var list1 = List.filled(6, 2);
   print(list1);
 
-  List list2 = List.filled(6, 3,growable: true);
+  List list2 = List.filled(6, 3, growable: true);
   list2.add(3);
   list2[1] = 4;
   list2[3] = 5;
@@ -21,4 +21,18 @@ void main(){
   print('index of 7 = ${list2.indexOf(7)}');
   print(list2);
 
+  List ab3 = List.from([1, 2, 3, 4]);
+  ab3.add(67);
+  print('ab3 = $ab3');
+
+  print('*********************************************');
+
+
+  List list4 = List.generate(11, (index) => index * 2);
+  list4.add(45);
+  print('list4 = $list4');
 }
+ // print("first element in list6 = ${list6.first}");
+  //print(list.con
+
+
---
+++ bin/mylist.dart
@@ -0,0 +1,8 @@
+void main(){
+  List a= [1,2,3,4,5];
+  for(int  i in a){
+    print(i);
+
+  }
+}
+
